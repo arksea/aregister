c26b4dc541d94b9c760775e732f73782387ff3ce
xiaohaixing <arksea@gmail.com>
2018-05-23 14:17:01 +0800

    初始化项目

---
+++ aregister-web/src/main/groovy/net/arksea/ereader/server/system/SystemFactory.java
@@ -0,0 +1,17 @@
+package net.arksea.ereader.server.system;
+
+import akka.actor.ActorSystem;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+
+/**
+ *
+ * Created by xiaohaixing on 2017/07/22.
+ */
+@Component
+public class SystemFactory {
+
+}
